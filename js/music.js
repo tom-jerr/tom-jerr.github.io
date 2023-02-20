e9ba00472d96176d4f301abb498cb30c9ddad403
tom-jerr <2584074296@qq.com>
2023-02-20 16:10:39 +0800

    Site updated: 2023-02-20 16:10:39

--- js/music.js
+++ js/music.js
@@ -1,4 +1,4 @@
-const ap = new APlayer({
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
 	autoplay: true, //自动播放
